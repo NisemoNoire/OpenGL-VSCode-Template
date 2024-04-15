d689a71f493ab1d9e63a338151b8563ba15755c3
NisemoNoire <dokupe0818@gmail.com>
2024-04-15 20:07:29 +0800

    Update main.cpp

--- src/01_window/main.cpp
+++ src/01_window/main.cpp
@@ -6,13 +6,14 @@
 #include <ctime>
 #include <iostream>
 #define STB_IMAGE_IMPLEMENTATION
+#include <filesystem>
 #include <stb_image.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_WIDTH  = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 int main()
@@ -43,8 +44,9 @@ int main()
     }
 
     /*-------------------------------↓ Custom ↓---------------------------------*/
-    Shader firstShader(R"(D:\CODE\OpenGLxvsc\src\01_window\shader.vs)", "D:\\CODE\\OpenGLxvsc\\src\\01_window\\shader.fs");
-
+    std::filesystem::path vs("../src/01_window/shader.vs");
+    std::filesystem::path fs("../src/01_window/shader.fs");
+    Shader firstShader(std::filesystem::absolute(vs).string().c_str(), std::filesystem::absolute(fs).string().c_str());
     /* 顶点数据 */
     float vertices[] = {
         // positions          // colors           // texture coords
@@ -95,7 +97,9 @@ int main()
     /* 加载图像并绑定 */
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true);
-    unsigned char* data = stbi_load("D:\\CODE\\OpenGLxvsc\\src\\01_window\\container.jpg", &width, &height, &nrChannels, 0);
+    vs = "../src/01_window/container.jpg";
+
+    unsigned char* data = stbi_load(std::filesystem::absolute(vs).string().c_str(), &width, &height, &nrChannels, 0);
     if (data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
@@ -116,8 +120,9 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+    vs = "../src/01_window/awesomeface.png";
     /* 加载图像并绑定 */
-    data = stbi_load("D:\\CODE\\OpenGLxvsc\\src\\01_window\\awesomeface.png", &width, &height, &nrChannels, 0);
+    data = stbi_load(std::filesystem::absolute(vs).string().c_str(), &width, &height, &nrChannels, 0);
     if (data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
